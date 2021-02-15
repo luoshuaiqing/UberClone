ed587ad726a5420c7df914ae6685b9d19ec1b9c1
Shuaiqing Luo <shuaiqin@usc.edu>
2021-02-15 13:35:02 -0800

    lecture 1 finished

---
+++ UberClone/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  UberClone
+//
+//  Created by 罗帅卿 on 2/15/21.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+}
